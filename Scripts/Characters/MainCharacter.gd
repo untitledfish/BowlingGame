6d344307cf3f82692d976b5fb28f49d602ff4850
Narmauda <157421741+Narmauda@users.noreply.github.com>
2025-04-01 01:07:49 -0400

    Smoothed out dashing

--- Scripts/Characters/MainCharacter.gd
+++ Scripts/Characters/MainCharacter.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 #Variables
 const walk_speed = 300
 const run_speed = 500
-const dash_speed = 2000
+const dash_speed = 3000
 const jump_height = -500
 var last_input = 0
 var can_dash = true
+
+#State variables
+var dashing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,9 +18,9 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	#Move left and right. If Shift is held, move faster
 	var move_input = Input.get_axis("ui_left", "ui_right")
-	if move_input and Input.is_action_pressed("Run"):
+	if move_input and Input.is_action_pressed("Run") and dashing == false:
 		velocity.x = move_input * run_speed
-	elif move_input:
+	elif move_input and dashing == false:
 		velocity.x = move_input * walk_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0 , walk_speed)
@@ -30,13 +33,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = jump_height
 	
-	#Dashing, feels too instant so will work on it more (K)
-	if Input.is_action_just_pressed("Dash") and move_input and (can_dash == true):
+	#Dashing (K)
+	if Input.is_action_just_pressed("Dash") and move_input and (can_dash == true) and dashing == false:
 		last_input = move_input
-		velocity = velocity.move_toward(Vector2(last_input * dash_speed, 0), dash_speed)
+		dashing = true
+		velocity.x = dash_speed * last_input
 		velocity.y = -9.8
-		await get_tree().create_timer(0.5).timeout
+		await get_tree().create_timer(0.15).timeout
 		can_dash = false
+		dashing = false
 		$DashCD.start()
 		
 	move_and_slide()
