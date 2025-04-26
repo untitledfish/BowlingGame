66df5f35f37f20c7f1f655c43f49514e0c6108c5
Bobble Hat <lu.dru1459@gmail.com>
2025-04-25 22:42:41 -0400

    fully reverted merge

--- Scripts/Menu/LevelSelect.gd
+++ Scripts/Menu/LevelSelect.gd
@@ -4,9 +4,9 @@ func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Maps/level_1.tscn")
 
 func _on_level_2_pressed() -> void:
-	#get_tree().change_scene_to_file()
+	#get_tree().change_scene_to_file("res://Scenes/Maps/level_2.tscn")
 	pass
 
 func _on_level_3_pressed() -> void:
-	#get_tree().change_scene_to_file()
+	#get_tree().change_scene_to_file("res://Scenes/Maps/level_3.tscn")
 	pass
